cbaa8fcb0be2d08d19ea3677c32a73137cf2732f
CAN7 <39960526+CAN1177@users.noreply.github.com>
2021-02-02 22:51:47 +0800

    session实现登录

--- node-blog/app.js
+++ node-blog/app.js
@@ -2,6 +2,15 @@ const handleBlogRouter = require("./src/router/blog");
 const handleUserRouter = require("./src/router/user");
 const queryString = require("querystring");
 
+// 获取 cookie 的过期时间
+const getCookieExpires = () => {
+  const d = new Date();
+  d.setTime(d.getTime() + 12 * 60 * 60 * 1000);
+  return d.toGMTString();
+};
+// 全局session data
+const SESSION_DATA = {};
+
 //异步处理post data
 const getPostData = (req) => {
   return new Promise((resolve, reject) => {
@@ -34,29 +43,67 @@ const serverHeader = (req, res) => {
   // 处理路径path
   const url = req.url;
   req.path = url.split("?")[0];
+  // 解析query
+  req.query = queryString.parse(url.split("?")[1]);
 
+  // 解析cookie
+  req.cookie = {};
+  const cookieStr = req.headers.cookie || "";
+  cookieStr.split(";").forEach((item) => {
+    if (!item) {
+      return;
+    }
+    let arr = item.split("=");
+    const key = arr[0].trim();
+    const value = arr[1].trim();
+    req.cookie[key] = value;
+  });
+
+  // 解析session
+  let needSession = false;
+  let userId = req.cookie.userid;
+  if (userId) {
+    if (SESSION_DATA[userId]) {
+      req.session = SESSION_DATA[userId];
+    } else {
+      SESSION_DATA[userId] = {};
+      req.session = SESSION_DATA[userId];
+    }
+  } else {
+    needSession = true;
+    userId = `${Date.now()}_${Math.random()}`;
+    SESSION_DATA[userId] = {};
+    req.session = SESSION_DATA[userId];
+  }
   //处理post data
-
   getPostData(req).then((postData) => {
     req.body = postData;
     //处理blog路由
     const blogResult = handleBlogRouter(req, res);
     if (blogResult) {
       blogResult.then((blogData) => {
+        if (needSession) {
+          res.setHeader(
+            "Set-Cookie",
+            `userid=${userId}; path=/; httpOnly; expires=${getCookieExpires()}`
+          );
+        }
         res.end(JSON.stringify(blogData));
       });
       return;
     }
 
     // 处理user路由
-    // const userData = handleUserRouter(req, res);
-    // if (userData) {
-    //   res.end(JSON.stringify(userData));
-    //   return;
-    // }
     const userResult = handleUserRouter(req, res);
     if (userResult) {
       userResult.then((userData) => {
+        console.log("userData: ", userData);
+        if (needSession) {
+          res.setHeader(
+            "Set-Cookie",
+            `userid=${userId}; path=/; httpOnly; expires=${getCookieExpires()}`
+          );
+        }
         res.end(JSON.stringify(userData));
       });
       return;
@@ -67,9 +114,6 @@ const serverHeader = (req, res) => {
     res.write("404 not found");
     res.end();
   });
-
-  // 解析query
-  req.query = queryString.parse(url.split("?")[1]);
 };
 
 module.exports = serverHeader;
--- node-blog/src/router/user.js
+++ node-blog/src/router/user.js
@@ -1,14 +1,19 @@
-const loginCheck = require('../controller/user')
+const login = require('../controller/user')
 const { SuccessModel, ErrorModel } = require("../model/resModel");
+const queryString = require("querystring");
+
 
 const handleUserRouter = (req, res) =>{
 	const method = req.method 
-
-	if (method=="POST" && req.path=='/api/user/login') {
-		const {username,password} = req.body
-		let result = loginCheck(username,password)
+	if (method=="GET" && req.path=='/api/user/login') {
+		const {username,password} = req.query
+		let result = login(username,password)
 		return result.then((data)=>{
 			if(data.username) {
+				//设置session
+				req.session.username = data.username
+				req.session.realname = data.realname
+				console.log('req.session: ', req.session);
 				return new SuccessModel();
 			}
 			return new ErrorModel("login fail");
